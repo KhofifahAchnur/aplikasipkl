28ce446c3a3878091795aebcb2c036e600d0cc41
Rizky Kurniawan <contact.rizkykurniawan@gmail.com>
2022-01-30 15:58:24 +0800

    fix histori kondisi bug

--- application/models/M_kondisi.php
+++ application/models/M_kondisi.php
@@ -2,44 +2,44 @@
 
 class M_kondisi extends CI_model
 {
-    public function lihat()
-    {
-        $this->db->select('history_kondisi.id, aset.nama_barang, aset.kode_barang, aset.kondisi, history_kondisi.tanggal');
-        $this->db->from('history_kondisi');
-        $this->db->join('aset', 'aset.id = history_kondisi.aset_id ');
-        $this->db->order_by('history_kondisi.id', 'DESC');
-        return $this->db->get()->result_array();
+	public function lihat()
+	{
+		$this->db->select('history_kondisi.id, aset.nama_barang, 
+		aset.kode_barang, history_kondisi.kondisi, history_kondisi.tanggal');
+		$this->db->from('history_kondisi');
+		$this->db->join('aset', 'aset.id = history_kondisi.aset_id ');
+		$this->db->order_by('history_kondisi.id', 'DESC');
+		return $this->db->get()->result_array();
+	}
 
-    }
+	public function proses_tambah()
+	{
+		$data = [
+			"aset_id" => $this->input->post('nama_barang', true),
+			"tanggal" => date('Y-m-d'),
+			"kondisi" => $this->input->post('kondisi', true)
+		];
 
-    public function proses_tambah()
-    {
-        $data = [
-            "aset_id" => $this->input->post('nama_barang', true),
-            "tanggal" => date('Y-m-d')
-           
-        ];
-    
-        $this->db->insert('history_kondisi', $data);
-    }
+		$this->db->insert('history_kondisi', $data);
+	}
 
-    public function updatekondisi($id)
-    {
-        $data = [
-            "kondisi" => $this->input->post('kondisi', true)
-        ];
-        $this->db->where('id', $id);
-        $this->db->update('aset', $data);
-    }
- 
- 
-    public function getKondisiById($id)
-    {
-        return $this->db->get_where('history_kondisi', ['id' => $id])->row_array();
-    }
+	public function updatekondisi($id)
+	{
+		$data = [
+			"kondisi" => $this->input->post('kondisi', true)
+		];
+		$this->db->where('id', $id);
+		$this->db->update('aset', $data);
+	}
 
-    // public function jumlah()
-    // {
-    //     return $this->db->get('history_kondisi')->num_rows();
-    // }
+
+	public function getKondisiById($id)
+	{
+		return $this->db->get_where('history_kondisi', ['id' => $id])->row_array();
+	}
+
+	// public function jumlah()
+	// {
+	//     return $this->db->get('history_kondisi')->num_rows();
+	// }
 }
